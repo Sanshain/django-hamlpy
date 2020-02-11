8b8b27645b4cf30814089278afed3a8bdff96916
DigmaBook <digital-mag@ya.ru>
2020-02-11 22:54:05 +0300

    modify components_save

--- hamlpy/__init__.py
+++ hamlpy/__init__.py
@@ -16,6 +16,10 @@ try:
 
         HAML_UNIT.UNITS = settings.LIGHT_UNITS
 
+        HAML_UNIT.STYLE_PREPROCS = (
+            settings.STYLE_PREPROCS if hasattr(settings, 'STYLE_PREPROCS') else []
+        )
+
         print 'init HAML_UNIT'
 
 except Exception as e:
@@ -25,6 +29,9 @@ except Exception as e:
     print e.message
 
 
+
+import os
+print os.path.abspath('')
 print 'HAML_EXTENSIONS'
 
 
