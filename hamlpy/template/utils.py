e02ddbda816a0c459146c8b5268e6ed670b3649a
DigmaBook <digital-mag@ya.ru>
2020-02-11 18:07:27 +0300

    subcommit

--- hamlpy/template/utils.py
+++ hamlpy/template/utils.py
@@ -23,27 +23,51 @@ def package_contents(package):
     return contents
 
 
+def type_save(content, template_type, content_type, ext, optional=None):
+
+    '''
+    save type
+
+    template_type - subdirectory for saving (`components`,`fragments`,`pages`)
+    content_type - name of directory for saving (`style`,`js`)
+    ext - extension for saving (`js`,`css`, `less`)
+    optional - optional handle funcs for process (for example for less compile)
+
+    '''
+
+    cs_path = os.path.join(static_path, content_type, template_type)
+
+    if not os.path.exists(cs_path): os.makedirs(cs_path)
+
+    style_flname = os.path.join(cs_path, base_name + '.' + ext or content_type)
+
+    with open(style_flname, 'w') as style_file: style_file.write(cs_content.encode('utf-8'))
+
 
 def convert(contents, origin):
 
 
     multi_content = contents.split(HAML_UNIT.UNITS['js'])
-    contents = multi_content[0]
+    content = multi_content[0]
 
-    jcs_content = multi_content[1] if len(multi_content) > 1 else ''
+    if len(multi_content) > 1: jcs_content = multi_content[1]
+    else:
+        return content
+
+
 
     print 'origin: ' + str(origin)
 
-    pathname_origin, filename_origin = os.path.split(origin.__str__()) # [`/templates/pages`, `tmpl.haml`]
-    base_name = filename_origin.rsplit('.',1)[0]                       # `tmpl`
+    pathname_origin, filename_origin = os.path.split(origin.__str__())          # [`/templates/pages`, `tmpl.haml`]
+    base_name = filename_origin.rsplit('.',1)[0]                                # `tmpl`
 
 
 
-    base_path, template_type = os.path.split(pathname_origin)          # [`/templates`,`pages`]
+    base_path, template_type = os.path.split(pathname_origin)                   # [`/templates`,`pages`]
 
     if template_type in ('components', 'fragments', 'pages'): pass
     elif template_type == 'templates':  template_type = ''
-    else:                                                           # other pathes
+    else:                                                                       # other pathes
         template_type = 'pages'
 
     if template_type:
@@ -58,27 +82,25 @@ def convert(contents, origin):
 
         jcs_content = jcs_content.split(HAML_UNIT.UNITS['style'])
 
+
+        types = (2*('js',), ('style','css'))
+
+        dct = dict(zip(jcs_content, types))
+
+        for _content in dct:
+
+            type_save(_content, template_type, *dct[_content])
+
+
+
+
+
         js_content = jcs_content[0]
 
         cs_content = jcs_content[1] if len(jcs_content) > 1 else ''
 
-        if js_content:
+        if js_content: type_save(cs_content, template_type, 'js')
 
-            js_path = os.path.join(static_path, 'js', template_type)
+        if cs_content: type_save(cs_content, template_type, 'style','css')
 
-            if not os.path.exists(js_path): os.makedirs(js_path)
-
-            js_flname = os.path.join(js_path, base_name + '.js')
-
-            with open(js_flname, 'w') as js_file: js_file.write(js_content.encode('utf-8'))
-
-        if cs_content:
-
-            cs_path = os.path.join(static_path, 'style', template_type)
-
-            if not os.path.exists(cs_path): os.makedirs(cs_path)
-
-            style_flname = os.path.join(cs_path, base_name + '.css')
-
-            with open(style_flname, 'w') as style_file: style_file.write(cs_content.encode('utf-8'))
-
+    return content
